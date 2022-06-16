64fa70fb6d96e602e05285f29c079c69ea45a31f
ManivishalBurra <72870001+ManivishalBurra@users.noreply.github.com>
2022-06-16 22:02:15 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import Button from "../ChickenApp/src/components/Button";
+import LoginScreen from "react-native-login-screen";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginScreen
+        logoImageSource={"https://www.google.com/imgres?imgurl=https%3A%2F%2Fw1.pngwing.com%2Fpngs%2F848%2F36%2Fpng-transparent-bird-logo-rooster-chicken-food-chicken-as-food-poussin-doner-kebab-text.png&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-vieis&tbnid=qb3nW4jNW8Wc7M&vet=10CAkQxiAoC2oXChMI0Izdoaqy-AIVAAAAAB0AAAAAEAY..i&docid=pDFNaM152Cr9CM&w=920&h=518&itg=1&q=chicken%20logo%20in%20red%20background&ved=0CAkQxiAoC2oXChMI0Izdoaqy-AIVAAAAAB0AAAAAEAY"}
+        onLoginPress={() => {}}
+        onSignupPress={() => {}}
+        onEmailChange={() => {}}
+        onPasswordChange={() => {}}
+        style={{ backgroundColor: "#FF5C58" }}
+        loginButtonStyle={{ backgroundColor: "black" }}
+        dividerStyle={{backgroundColor:'black'}}
+      />
+      <StatusBar styles="auto" />
     </View>
   );
 }
@@ -13,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FF5C58",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginBox: {
+    width: "80%",
+    height: "50%",
+    backgroundColor: "#FFF8E5",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    backgroundColor: "orange",
+    width: "75%",
+    borderRadius: 10,
+    height: 50,
+    padding: 10,
   },
 });
---
+++ src/components/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+ 
+export default function Button({ text, onPress,width, height}) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+            <View style={[styles.button,{width: width, height: height}]}>
+                <Text style={styles.buttonText}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width:'100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    button: {
+        borderRadius: 8,
+        paddingVertical: 14,
+        paddingHorizontal: 10,
+        backgroundColor: 'black',
+        margin:5
+    },
+    buttonText: {
+        color:"white",
+        textTransform: 'uppercase',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+});
